9e2bac91dc5dfff9846658a266ae7fb95e43e541
PaarkG <2007kdogrules@gmail.com>
2026-06-02 22:47:11 -0400

    Initial project setup and swerve import

---
+++ src/main/java/frc/robot/subsystems/drive/SwerveConstants.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.drive;
+
+public class SwerveConstants {
+    // TRANSLATION PID
+    public static final double kTranslationP = 5;
+    public static final double kTranslationI = 0;
+    public static final double kTranslationD = 0;
+    // ROTATION PID
+    public static final double kRotationP = 7;
+    public static final double kRotationI = 0;
+    public static final double kRotationD = 0;
+}
---
+++ src/main/java/frc/robot/utils/vision/AllianceFlip.java
@@ -0,0 +1,32 @@
+package frc.robot.utils.vision;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
+public class AllianceFlip {
+    public static boolean shouldFlip() {
+        return DriverStation.getAlliance().isPresent()
+                && DriverStation.getAlliance().get() == Alliance.Red;
+    }
+
+    public static Translation2d flip(Translation2d position) {
+        if (shouldFlip()) {
+            return new Translation2d(
+                    FieldConstants.fieldLength,
+                    FieldConstants.fieldWidth).minus(position);
+        } else {
+            return position;
+        }
+    }
+
+    public static Rotation2d flip(Rotation2d rotation) {
+        if (shouldFlip()) {
+            return Rotation2d.k180deg.plus(rotation);
+        } else {
+            return rotation;
+        }
+    }
+
+}
